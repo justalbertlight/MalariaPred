38b850965a65517e028e67e4d53cdf81e1ead157
justalbertlight <104940651+justalbertlight@users.noreply.github.com>
2025-03-25 20:29:05 +0100

    app.py

--- app.py
+++ app.py
@@ -27,33 +27,33 @@ diabetes_model = pickle.load(open(f'{working_dir}/saved_models/malariaodel.sav',
 
 # Diabetes Prediction Page
     # page title
-    st.title('Malaria Prediction using ML')
+  st.title('Malaria Prediction using ML')
 
     # getting the input data from the user
-    col1, col2, col3 = st.columns(3)
+  col1, col2, col3 = st.columns(3)
 
-    with col1:
+  with col1:
         Pregnancies = st.text_input('Number of Pregnancies')
 
-    with col2:
+  with col2:
         Glucose = st.text_input('Glucose Level')
 
-    with col3:
+  with col3:
         BloodPressure = st.text_input('Blood Pressure value')
 
-    with col1:
+  with col1:
         SkinThickness = st.text_input('Skin Thickness value')
 
-    with col2:
+  with col2:
         Insulin = st.text_input('Insulin Level')
 
-    with col3:
+  with col3:
         BMI = st.text_input('BMI value')
 
-    with col1:
+  with col1:
         DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
 
-    with col2:
+  with col2:
         Age = st.text_input('Age of the Person')
 
 
