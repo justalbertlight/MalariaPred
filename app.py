6b41592d5af20295ae012b32baecdf53c8ab03c4
justalbertlight <104940651+justalbertlight@users.noreply.github.com>
2025-03-27 19:01:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from streamlit_option_menu import option_menu
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
