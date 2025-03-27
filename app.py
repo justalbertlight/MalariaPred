270bdb2b035e96c0b7500a84c9a8a0099d2dccef
justalbertlight <104940651+justalbertlight@users.noreply.github.com>
2025-03-27 18:57:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import pickle
 import streamlit as st
 import numpy as np
 from streamlit_option_menu import option_menu
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 # Set page configuration
 st.set_page_config(page_title="Malaria in Pregnancy Prediction",
