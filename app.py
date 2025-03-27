e3fefc20c0c1df05fc052b3cf10aed2a386ff6e4
justalbertlight <104940651+justalbertlight@users.noreply.github.com>
2025-03-27 18:36:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,8 +76,7 @@ if st.button('Malaria Test Result'):
         #inputdata = [float(x) for x in user_input]
 
         #This will no longer be the prediction
-  .     #prediction = malariamodel.predict([inputdata])
-
+  .     # 
 
 
        #changng the input data to numpy array
