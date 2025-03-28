d7d488470735dfb148f1e6ffb0e09950675b1480
justalbertlight <104940651+justalbertlight@users.noreply.github.com>
2025-03-28 13:21:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 
 # Set page configuration
-st.set_page_config(page_title="Malaria in Pregnancy Prediction",
+st.set_page_config(page_title="Prediction of Malaria in Pregnancy",
                    layout="wide",
                    page_icon="🤰")
 
@@ -23,30 +23,20 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 # loading the saved models
 
 malariamodel = pickle.load(open(f'{working_dir}/saved_models/malariamodelnew.sav', 'rb'))
-# saved_models/malariamodelnew.sav'
-#heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
-
-#parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
-
-# sidebar for navigation
-#with st.sidebar:
-    #
 
 
-# Diabetes Prediction Page
-    # page title
-st.title('Malaria in Pregnancy Prediction using Machine Learning')
+# page title
+st.title('Prediction of Prevalence of Malaria in Pregnancy using Machine Learning Technique')
 
-    # getting the input data from the user
-
-
+# getting the input data from the user
 age = st.number_input('Age', min_value=15, max_value=50, step=1)
-gravida = st.number_input('Gravida', min_value=1, max_value=20, step=1)
-gestationalage = st.number_input('Gestational Age (months)', min_value=1, max_value=9, step=1)
-pastmalaria = st.radio("Past Malaria?", 
+gravida = st.number_input('Gravida(Number of times of pregnancy)', min_value=1, max_value=20, step=1)
+gestationalage = st.number_input('Gestational Age (How many months?)', min_value=1, max_value=9, step=1)
+pastmalaria = st.radio("Have you had malaria in the past?", 
                            ["No", "Yes", "Not Sure"], 
                            index=0)  # Default: "No"
-area = 1 if st.checkbox('Area?') else 0
+area = 1 if st.checkbox('Do you live in an area where mosquitoes are common?') else 0
+
 fever = 1 if st.checkbox('Fever?') else 0
 bodyaches = 1 if st.checkbox('Body Aches?') else 0
 chills = 1 if st.checkbox('Chills?') else 0
@@ -56,7 +46,8 @@ profuse = 1 if st.checkbox('Profuse Sweating?') else 0
 vomiting = 1 if st.checkbox('Vomiting?') else 0
 severeheadache = 1 if st.checkbox('Severe Headache?') else 0
 severechills = 1 if st.checkbox('Severe Chills?') else 0
-howoften = st.radio("How often do you get tested?", 
+
+howoften = st.radio("How often do you get tested/treated for malaria?", 
                         ["Never", "Occasionally", "Frequently"], 
                         index=0)  # Default: "Never"
 # Convert "Past Malaria" to numerical values
